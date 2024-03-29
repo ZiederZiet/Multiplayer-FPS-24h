126634ea7c143ba0247b832a047e8b5d83845415
ZiederZiet <ziederziet@gmail.com>
2024-03-29 12:50:14 +0100

    Player Setup n' Stuff

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,3 +1,4 @@
+using FishNet.Connection;
 using FishNet.Object;
 using System.Collections;
 using System.Collections.Generic;
@@ -5,15 +6,33 @@ using UnityEngine;
 
 public class Player : NetworkBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject[] m_nonLocalOnly;
+    [SerializeField] private Transform m_head;
+    [SerializeField] private Renderer m_renderer;
+    private Transform m_camera;
     void Start()
     {
-        Debug.Log("HI");
+        m_camera = Camera.main.transform;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (IsOwner)
+        {
+            m_camera.position = m_head.position;
+            m_camera.rotation = m_head.rotation;
+        }
+    }
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        for (int i = 0; i < m_nonLocalOnly.Length; i++)
+        {
+            m_nonLocalOnly[i].SetActive(IsOwner);
+        }
+        if (IsOwner)
+        {
+            m_renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+        }
     }
 }
