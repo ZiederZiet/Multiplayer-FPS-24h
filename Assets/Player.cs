ed3cef6fec20a3a9da9f1d3bf0d26ec467b8272f
ZiederZiet <ziederziet@gmail.com>
2024-03-29 12:36:26 +0100

    Initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,19 @@
+using FishNet.Object;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("HI");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
